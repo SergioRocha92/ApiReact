15e13ff109ec714112a18068d4a404c7877a0d80
SergioRocha92 <69970438+SergioRocha92@users.noreply.github.com>
2020-11-29 12:45:28 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './style/App.css'
+import Form from './component/form';
+
+class App extends React.Component{
+  
+  render() {
+    return (
+      <div>
+        {}
+
+        <Form/>
+      
+      
+        
+      </div>
+    );
+  }
+
+}
+
+
+export default App;
